bf9c50a0ddd949d00b1ff12a7c9a15be0cbf8ba9
Meadhbh O'Neill <meadhbhon@gmail.com>
2021-10-01 12:34:21 +0100

    include data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Sample Data for MPR Setting.
+#'
+#' @format A list with:
+#' \describe{
+#'   \item{x}{}
+#'   \item{y}{}
+#'   \item{beta_true}{}
+#'   \item{alpha_true}{}
+#'   ...
+#' }
+"sample_data_mpr"
