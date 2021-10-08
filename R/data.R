8a6d97f86692804b5d8d24cfc82accc386ce5fff
Meadhbh O'Neill <meadhbhon@gmail.com>
2021-10-08 12:09:41 +0100

    WIP

--- R/data.R
+++ R/data.R
@@ -1,11 +1,35 @@
-#' Sample Data for MPR Setting.
+#' Prostate Cancer Data
 #'
-#' @format A list with:
+#' A dataset, which come from a study by Stamey et al. (1989), examining the correlation between the level of prostate-specific
+#' antigen (PSA) and various clinical measures in men who were about the receive
+#' a radical prostatectomy.
+#'
+#' @format A data frame with 97 rows and 9 variables:
 #' \describe{
-#'   \item{x}{}
-#'   \item{y}{}
-#'   \item{beta_true}{}
-#'   \item{alpha_true}{}
-#'   ...
-#' }
-"sample_data_mpr"
+#'   \item{lcavol}{log(cancer volume (\eqn{cm^{3}}))}
+#'   \item{lweight}{log(prostate weight (g))}
+#'   \item{age}{age of the patient}
+#'   \item{lbph}{log(amount of benign prostatic hyperplasia (\eqn{cm^{2}}))}
+#'   \item{svi}{presence of seminal vesicle invasion (1=yes, 0=no)}
+#'   \item{lcp}{log(capsular penetration (cm))}
+#'   \item{gleason}{Gleason score}
+#'   \item{pgg45}{percentage of Gleason scores four of five}
+#'   \item{lpsa}{log(PSA (ng/mL))}
+#'   }
+#'
+#' @source \url{https://web.stanford.edu/~hastie/ElemStatLearn/datasets/prostate.data}
+#' @references
+#' Stamey, T. A., Kabalin, J. N., McNeal, J. E., Johnstone, I. M., Freiha, F., Redwine,
+#' E. A., and Yang, N. (1989). Prostate specific antigen in the diagnosis and treatment of
+#' adenocarcinoma of the prostate. ii. radical prostatectomy treated patients. The Journal
+#' of urology, 141(5):1076{1083}.
+#'
+"pcancer"
+
+#' Sniffer Data
+#'
+"sniffer"
+
+#' Boston House Price Data
+#'
+"bostonhouseprice"
